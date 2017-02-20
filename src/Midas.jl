8d17b0c0b79908bd9697c3668bbf9d8a12271d52
Mike Mull <mike.mull@gmail.com>
2017-02-20 15:22:24 -0800

    First version of build high-frequency lags

--- src/Midas.jl
+++ src/Midas.jl
@@ -1,5 +1,52 @@
 module Midas
 
 # package code goes here
+using TimeSeries
+
+@enum FREQ fday=1 fmonth=2 fquarter=3 funknown=4
+
+export mixedfrequency, datafreq, FREQ, fquarter, fmonth
+
+function mixedfrequency(lf_data, hf_data, xlag, horizon)
+
+  start_hf = find(x -> x >= lf_data.timestamp[1], hf_data.timestamp)[1]
+  start_hf = start_hf - xlag - horizon + 1
+
+  hf_gt = find(x -> x >= lf_data.timestamp[end], hf_data.timestamp)
+  if length(hf_gt) == 0
+    end_hf = length(hf_data)
+  else
+    end_hf = hf_gt[1]
+  end
+  end_hf -= horizon
+  # end hf
+  # num_obs
+  num_obs = length(lf_data)
+
+  hf_lags = zeros(num_obs * xlag)
+  for i in eachindex(hf_data.values[start_hf:end_hf])
+           hf_lags[i] = hf_data.values[i+start_hf-1]
+  end
+
+  hfv = transpose(reshape(hf_lags, (xlag, num_obs)))
+
+  return  TimeArray(lf_data.timestamp, hfv)
+end
+
+function datafreq(tsdata)
+
+  deltas = lag(tsdata).timestamp[1:end] - tsdata.timestamp[1:end-1]
+
+  mind = Int(minimum(deltas))
+
+  if 90 <= mind <= 92
+    return fquarter
+  elseif 28 <= mind <= 31
+    return fmonth
+  elseif mind == 1
+    return fday
+  end
+  return funknown
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
 using Midas
+using TimeSeries
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+months = [Date(2000, m, 1) for m in 1:12]
+quarters = [Date(2000, 3, 31), Date(2000, 6, 30),
+Date(2000, 9, 30), Date(2000, 12, 31)]
+lf_data = TimeArray(quarters, [1.0, 2.0, 3.0, 4.0])
+hf_data = TimeArray(months, collect(1.0:12.0))
+
+@test mixedfrequency(lf_data, hf_data, 3, 1).values[1, :] == [1.0, 2.0, 3.0]
+
+@test datafreq(lf_data) == fquarter
+@test datafreq(hf_data) == fmonth
+
+
+hf_data = readtimearray("./data/farmpay.csv", format="yyyy-mm-dd")
+lf_data = readtimearray("./data/gdp.csv", format="yyyy-mm-dd")
+
+@test mixedfrequency(lf_data, hf_data, 3, 1).values[1, :] == [43094.0,43396.0,43379.0]
+@test mixedfrequency(lf_data, hf_data, 3, 1).values[end, :] == [130328.0,130563.0,130757.0]
